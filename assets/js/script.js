54136b472b7720b11b9c50391347c0eab98d51e1
hernikplays <hernik27@gmail.com>
2020-02-27 17:12:27 +0100

    woo getting info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 const Mangadex = require("mangadex-api")
 
+
+document.getElementById("folder").value = __dirname;  // sets folder input to the current folder the EXE is located in
+
 function scrape(id, filePath){
-    
+    Mangadex.getManga(id).then(({ manga, chapter }) => {
+        console.log(manga)
+        let cover = manga.cover_url.replace("cdndex.com", "mangadex.org") //bad link in api, replace it with right
+        document.getElementById("cover").src = cover;
+        document.getElementById("name").innerHTML = manga.title 
+        document.getElementById("info").style.display = "contents"; //show info
+      }) //gets manga info
+
+
 }
