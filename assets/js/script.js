4f96ca337e8c1c9f506460fabf744ff8c23585a6
hernikplays <hernik27@gmail.com>
2020-02-27 18:11:07 +0100

    how to get chapter link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,64 @@
 const Mangadex = require("mangadex-api")
+const https = require('https');
 const fs = require('fs')
 
-document.getElementById("folder").value = __dirname;  // sets folder input to the current folder the EXE is located in
+document.getElementById("folder").value = __dirname; // sets folder input to the current folder the EXE is located in
+document.getElementById("lang").value = "English"; //set lang automatically because Im lazy
 
-function scrape(id, filePath){
-    Mangadex.getManga(id).then(({ manga, chapter }) => {
+function scrape(id, filePath) {
+    Mangadex.getManga(id).then(({
+        manga,
+        chapter
+    }) => {
         console.log(chapter)
         let cover = manga.cover_url.replace("cdndex.com", "mangadex.org") //bad link in api, replace it with right
         document.getElementById("cover").src = cover;
-        document.getElementById("name").innerHTML = manga.title 
+        document.getElementById("name").innerHTML = manga.title
         document.getElementById("info").style.display = "contents"; //show info
 
-        if(document.getElementById("selop").options[document.getElementById("selop").selectedIndex].value == "all"){
+        if (document.getElementById("selop").options[document.getElementById("selop").selectedIndex].value == "all") {
             var chaps = []
 
-            for (var i = 0; i < chapter.length ; i++){ //filters chapters to selected language
-                if (chapter[i].lang_name.toLowerCase() == document.getElementById("lang").value.toLowerCase()){
+            for (var i = 0; i < chapter.length; i++) { //filters chapters to selected language
+                if (chapter[i].lang_name.toLowerCase() == document.getElementById("lang").value.toLowerCase()) {
                     chaps.push(chapter[i])
                     console.log("Push success")
                 }
             }
 
             console.log(chaps)
+            let path = filePath.replace("/", "\\")
+            for (var i = 0; i < chaps.length; i++) {
+                Mangadex.getChapter(chaps[ind].id).then(chapter => {
+                    console.log(chapter)
+                  })
+            }
 
         }
-        
-        
-          
-      }) //gets manga info
+
+
+
+    }) //gets manga info
 
 
 }
+
+
+/*
+ if (chaps[i].volume == "" || chaps[i].chapter == "") {
+                    fs.mkdirSync(`${path}\\${manga.title}\\Vol. ? Ch. ${chaps[i].title} - ${chaps[i].group_name}`, {
+                        recursive: true
+                    })
+                } else {
+                    if (!fs.existsSync(`${path}\\${manga.title}\\Vol. ${chaps[i].volume} Ch. ${chaps[i].chapter} - ${chaps[i].group_name}`)) {
+                        fs.mkdirSync(`${path}\\${manga.title}\\Vol. ${chaps[i].volume} Ch. ${chaps[i].chapter} - ${chaps[i].group_name}`, {
+                            recursive: true
+                        })
+                    }
+                    if (i <= 9) {
+                        const file = fs.createWriteStream(`${path}\\${manga.title}\\Vol. ${chaps[i].volume} Ch. ${chaps[i].chapter} - ${chaps[i].group_name}\\0${i +1}.png`);
+                    } else {
+                        const file = fs.createWriteStream(`${path}\\${manga.title}\\Vol. ${chaps[i].volume} Ch. ${chaps[i].chapter} - ${chaps[i].group_name}\\${i +1}.png`);
+                    }
+                }
+                */
