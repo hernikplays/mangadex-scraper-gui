42b8c3e05df858ab2bba2f1aed657277b2729504
hernikplays <hernik27@gmail.com>
2020-02-27 17:33:56 +0100

    filtering chapters by language

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
 const Mangadex = require("mangadex-api")
-
+const fs = require('fs')
 
 document.getElementById("folder").value = __dirname;  // sets folder input to the current folder the EXE is located in
 
 function scrape(id, filePath){
     Mangadex.getManga(id).then(({ manga, chapter }) => {
-        console.log(manga)
+        console.log(chapter)
         let cover = manga.cover_url.replace("cdndex.com", "mangadex.org") //bad link in api, replace it with right
         document.getElementById("cover").src = cover;
         document.getElementById("name").innerHTML = manga.title 
         document.getElementById("info").style.display = "contents"; //show info
+
+        if(document.getElementById("selop").options[document.getElementById("selop").selectedIndex].value == "all"){
+            var chaps = []
+
+            for (var i = 0; i < chapter.length ; i++){ //filters chapters to selected language
+                if (chapter[i].lang_name.toLowerCase() == document.getElementById("lang").value.toLowerCase()){
+                    chaps.push(chapter[i])
+                    console.log("Push success")
+                }
+            }
+
+            console.log(chaps)
+
+        }
+        
+        
+          
       }) //gets manga info
 
 
